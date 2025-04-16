df6e50ddeb2adfd9cf08dcfb7e7b79804f86f4d0
Inuka Silva <inukasilva85@gmail.com>
2025-04-16 22:52:44 +0530

    rearranged dataset structure

--- train_model.py
+++ train_model.py
@@ -7,46 +7,58 @@ from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 
-data = []
-labels = []
+def load_images_from_folders(base_dir):
+    data = []
+    labels = []
+    for folder in os.listdir(base_dir):
+        folder_path = os.path.join(base_dir, folder)
+        if os.path.isdir(folder_path):
+            for file in os.listdir(folder_path):
+                file_path = os.path.join(folder_path, file)
+                img = cv2.imread(file_path)
+                img = cv2.resize(img, (64, 64))
+                data.append(img)
+                labels.append(folder)
+    return np.array(data), np.array(labels)
 
-dataset_path = "data/asl_dataset"
 
-for folder in os.listdir(dataset_path):
-    folder_path = os.path.join(dataset_path, folder)
-
-    for file in folder_path:
-        file_path = os.path.join(folder_path, file)
-
+def load_loose_images(base_dir):
+    data = []
+    filenames = []
+    for file in os.listdir(base_dir):
+        file_path = os.path.join(base_dir, file)
         img = cv2.imread(file_path)
         img = cv2.resize(img, (64, 64))
-
         data.append(img)
-        labels.append(folder)
+        filenames.append(file)
+    return np.array(data), filenames
 
-data = np.array(data)
-labels = np.array(labels)
+train_dir = "data/asl_dataset/train"
+X_train, y_train = load_images_from_folders(train_dir)
 
-data = data / 255.0
+test_dir = "data/asl_dataset/test"
+X_test, test_filenames = load_loose_images(test_dir)
+
+X_train = X_train / 255.0
+X_test = X_test / 255.0
 
 label_encoder = LabelEncoder()
-labels = label_encoder.fit_transform(labels)
+y_train = label_encoder.fit_transform(y_train)
 
-X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, random_state=42)
-
-model = Sequential()
-
-model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(64, 64, 3)))
-model.add(MaxPooling2D((2, 2)))
-
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D((2, 2)))
-
-model.add(Flatten())
-model.add(Dense(128, activation='relu'))
-model.add(Dense(26, activation='softmax'))
+model = Sequential([
+    Conv2D(32, (3, 3), activation='relu', input_shape=(64, 64, 3)),
+    MaxPooling2D((2, 2)),
+    Conv2D(64, (3, 3), activation='relu'),
+    MaxPooling2D((2, 2)),
+    Flatten(),
+    Dense(128, activation='relu'),
+    Dense(26, activation='softmax')
+])
 
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-history = model.fit(X_train, y_train, epochs=10, validation_data=(X_test, y_test))
+model.fit(X_train, y_train, epochs=10)
 
+model.save('asl_model.h5')
+
+print("Test filenames:", test_filenames)
